677e54dfadb43c0cbc17f6018ea5f75aa9d76149
WE-hai <943239470@qq.com>
2019-10-25 22:04:12 +0800

    java

---
+++ 10-25/VariableDemo1.java
@@ -0,0 +1,55 @@
+public class VariableDemo1{
+	public static void main(String[] args){
+		int a;
+		a = 9;
+		System.out.println(a);
+		int minInt = Integer.MIN_VALUE;
+		int maxInt = Integer.MAX_VALUE;
+		System.out.println(minInt);
+		System.out.println(maxInt);
+		
+		short minShort = Short.MIN_VALUE;
+		short maxshort = Short.MAX_VALUE;
+		System.out.println(minShort);
+		System.out.println(maxshort);
+		
+		long minLong = Long.MIN_VALUE;
+		long maxLong = Long.MAX_VALUE;
+		System.out.println(minLong);
+		System.out.println(maxLong);
+		
+		byte minbyte = Byte.MIN_VALUE;
+		byte maxbyte = Byte.MAX_VALUE;
+		System.out.println(minbyte);
+		System.out.println(maxbyte);
+		
+		//short minShortSub1 = 
+		
+		
+		
+		int bInt = 10;
+		System.out.println(bInt);
+		bInt = 0b101;
+		System.out.println(bInt);
+		bInt = 011;
+		System.out.println(bInt);
+		bInt = 0x3F;
+		System.out.println(bInt);
+		
+		long cLong = 12L;
+		System.out.println(cLong);
+		cLong = 0b11;
+		System.out.println(cLong);
+		cLong = 013;
+		System.out.println(cLong);
+		cLong = 0x45;
+		System.out.println(cLong);
+		
+		
+		char aChar = 'a';
+		System.out.println(aChar);
+		char bChar = '家';
+		System.out.println(bChar);
+		
+	}
+}
