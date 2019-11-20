fcd3af12b8a38772375de35aa1940bb15e017cbe
WE-hai <943239470@qq.com>
2019-11-20 22:20:04 +0800

    add

---
+++ 11-20/src/Node.java
@@ -0,0 +1,12 @@
+class Node {
+    int val;
+    Node next;
+
+    Node(int val,Node next) {
+        this.val = val;
+        this.next = next;
+    }
+    Node(int val) {
+        this(val,null);
+    }
+}
