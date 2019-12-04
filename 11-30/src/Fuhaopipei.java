282604370d3dc3ecb9bf32c064803cd85f4eb088
WE-hai <943239470@qq.com>
2019-12-04 20:22:26 +0800

    add

---
+++ 11-30/src/Fuhaopipei.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Stack;
+
+public class Fuhaopipei {
+
+    public boolean isValid(String s) {
+        final char[][] arr = {{'(',')'},{'[',']'},{'{','}'}};
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i < s.length();i++) {
+            char cur
+            int flag = 0;
+            for(int j = 0;j < arr.length;j++) {
+
+            }
+       }
+        return st.isEmpty();
+    }
+}
