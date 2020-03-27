b0185f0ba61a88885b98c55c94f4f82a85c11daf
WE-hai <943239470@qq.com>
2020-03-27 17:41:12 +0800

    线程

---
+++ 3-26/src/MThread.java
@@ -0,0 +1,10 @@
+public class MThread extends Thread {
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public static void main(String[] args) {
+        MThread t = new MThread();
+    }
+}
