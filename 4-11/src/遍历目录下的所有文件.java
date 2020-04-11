06c82084f4020b6b25c28f8a47874d18e1dcc1de
WE-hai <943239470@qq.com>
2020-04-11 17:45:06 +0800

    遍历目录下的所有文件

---
+++ 4-11/src/遍历目录下的所有文件.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 遍历目录下的所有文件 {
+    public static void main(String[] args) {
+        String path = "F:\\比特\\java-code\\java_work\\4-11";
+        File root = new File(path);
+
+        深度优先的遍历(root);
+        System.out.println();
+        广度优先的遍历(root);
+    }
+
+    private static void 广度优先的遍历(File root) {
+        Queue<File> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            File file = queue.poll();
+            System.out.println(file.getAbsolutePath());
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null) {
+                    for (File f : files) {
+                        queue.offer(f);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void 深度优先的遍历(File root) {
+        System.out.println(root.getAbsolutePath());
+
+        // root 不是文件夹 OR root 空文件夹 -> 叶子
+        if (root.isFile()) {
+            return;
+        }
+
+        if (!root.isDirectory()) {
+            return;
+        }
+
+        File[] files = root.listFiles();
+        if (files ==  null) {
+            return;
+        }
+
+        if (files.length == 0) {
+            return;
+        }
+
+        for (File file : files) {
+            深度优先的遍历(file);
+        }
+    }
+}
