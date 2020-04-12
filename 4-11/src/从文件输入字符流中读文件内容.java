5f4760345c5b97ed15b44b37b447fb0752d626ea
WE-hai <943239470@qq.com>
2020-04-12 14:18:16 +0800

    IO

---
+++ 4-11/src/从文件输入字符流中读文件内容.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class 从文件输入字符流中读文件内容 {
+    public static void main(String[] args) throws IOException {
+        try (InputStream is = new FileInputStream("有中文的文件.txt")) {
+            try (Reader reader = new InputStreamReader(is, "UTF-8")) {
+                StringBuilder sb = new StringBuilder();
+                char[] buffer = new char[1];
+                while (true) {
+                    int len = reader.read(buffer);
+                    if (len == -1) {
+                        break;
+                    }
+                    sb.append(buffer, 0, len);
+                }
+
+                System.out.println(sb.toString());
+            }
+        }
+    }
+}
