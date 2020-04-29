d7d919da87dbd852975fd70f5fed906e1d86110a
WE-hai <943239470@qq.com>
2020-04-29 14:56:07 +0800

    servlet

---
+++ 4-28/src/com/bit/Login.java
@@ -0,0 +1,26 @@
+package com.bit;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        resp.setContentType("Text/plain; charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        if (username != null && username.equals("weihaibo") && password != null && password.equals("980622"))  {
+            writer.println("登陆成功");
+            System.out.println("登录成功");
+        } else {
+            // 登录失败
+            writer.println("登录失败");
+            System.out.println("登录失败");
+        }
+    }
+}
