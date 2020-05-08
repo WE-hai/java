b2d17e7abc90062be77626810482b8177239b7af
WE-hai <943239470@qq.com>
2020-05-08 14:04:20 +0800

    add

---
+++ 5-6/src/com/bit/User.java
@@ -0,0 +1,15 @@
+package com.bit;
+
+public class User {
+    String username;
+    String nickname;
+    String password;
+    long balance;
+
+    public User(String username, String nickname, String password, long balance) {
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+        this.balance = balance;
+    }
+}
