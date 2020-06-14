f3a18e30f70998a20d1838489ab16e08674fae63
WE-hai <943239470@qq.com>
2020-06-14 13:20:12 +0800

    user

---
+++ 6-8 spring/ee-study/src/main/java/wei/model/User.java
@@ -0,0 +1,50 @@
+package wei.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private String username;
+    private String password;
+    private Date birthday;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
