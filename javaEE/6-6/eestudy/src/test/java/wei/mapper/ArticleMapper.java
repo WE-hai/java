a75d1bbce6a34ac039a6620964b0665cd8967fb6
WE-hai <943239470@qq.com>
2020-08-30 13:26:03 +0800

    add EE

---
+++ javaEE/6-6/eestudy/src/test/java/wei/mapper/ArticleMapper.java
@@ -0,0 +1,16 @@
+package wei.mapper;
+
+import java.util.List;
+import wei.model.Article;
+
+public interface ArticleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Article record);
+
+    Article selectByPrimaryKey(Integer id);
+
+    List<Article> selectAll();
+
+    int updateByPrimaryKey(Article record);
+}
